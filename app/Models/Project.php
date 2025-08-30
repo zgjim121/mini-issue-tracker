193c6b615d936ca2b49207f1b57cb541447a56fd
zgjim neziraj <zgjimneziraj@Mac.fritz.box>
2025-08-30 15:42:20 +0200

    adding relationships and adding the issue_tag table

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class project extends Model
+class Project extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function issues(): HasMany
+    {
+        return $this->hasMany(Issue::class);
+    }
 }
